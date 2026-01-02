69bd340b97c2f6c2b5699c643684d7051b80f322
Terdoo Nelson <nelsonterdoo1000@gmail.com>
2026-01-02 16:51:31 +0100

    fix BASE_DIR NameError in settings

--- cysm/settings.py
+++ cysm/settings.py
@@ -21,9 +21,10 @@ env = environ.Env(
     # set casting, default value
     DEBUG=(bool, False)
 )
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
