920b2fed097909c2d0eaf7fdc96468ec9b6aadbd
Terdoo Nelson <nelsonterdoo1000@gmail.com>
2026-01-04 18:00:17 +0100

    increased the data that should be exported from the import-export module

--- registeration/resources.py
+++ registeration/resources.py
@@ -4,8 +4,8 @@ from .models import Attendee, Registrar
 class AttendeeResource(resources.ModelResource):
     class Meta:
         model = Attendee
-        fields = ('surname', 'other_name', 'email', 'phone') 
-        export_order = ('surname', 'other_name', 'email', 'phone')  
+        fields = ('surname', 'other_name', 'email', 'phone', 'cys_code','church') 
+        export_order = ('surname', 'other_name', 'email', 'phone', 'cys_code')  
 
 class RegistrarResource(resources.ModelResource):
     class Meta:
