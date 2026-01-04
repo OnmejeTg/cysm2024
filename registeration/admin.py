7948aba7adc6943a353b9c569a3aa7372b199084
Terdoo Nelson <nelsonterdoo1000@gmail.com>
2026-01-04 11:43:41 +0100

    corrected the syntax error in the admin of registeration app

--- registeration/admin.py
+++ registeration/admin.py
@@ -4,13 +4,13 @@ from . models import Attendee, Registrar
 admin.site.register(Attendee)
 admin.site.register(Registrar)
 
-
+@admin.register(Attendee)
 class AttendeeAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'email', 'phone', 'created_at')  
     search_fields = ('first_name', 'last_name', 'email', 'phone','cys_code')
     list_filter = ('created_at',)
 
-
+@admin.register(Registrar)
 class RegistrarAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'email', 'phone', 'created_at')  
     search_fields = ('first_name', 'last_name', 'email', 'phone','cys_code')
