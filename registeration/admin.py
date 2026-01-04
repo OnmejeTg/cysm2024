0adecd28a225453e2e0166c38e6f50b8f843a495
Terdoo Nelson <nelsonterdoo1000@gmail.com>
2026-01-04 11:51:41 +0100

    fixed the wrong reference of the attendee and registrar models

--- registeration/admin.py
+++ registeration/admin.py
@@ -4,12 +4,31 @@ from . models import Attendee, Registrar
 
 @admin.register(Attendee)
 class AttendeeAdmin(admin.ModelAdmin):
-    list_display = ('first_name', 'last_name', 'email', 'phone', 'created_at')  
-    search_fields = ('first_name', 'last_name', 'email', 'phone','cys_code')
+    list_display = ('surname', 'other_name', 'email', 'phone', 'created_at')  
+    search_fields = ('surname', 'other_name', 'email', 'phone', 'cys_code')
     list_filter = ('created_at',)
 
 @admin.register(Registrar)
 class RegistrarAdmin(admin.ModelAdmin):
-    list_display = ('first_name', 'last_name', 'email', 'phone', 'created_at')  
-    search_fields = ('first_name', 'last_name', 'email', 'phone','cys_code')
-    list_filter = ('created_at',)   
+    list_display = ('get_surname', 'get_other_name', 'get_email', 'get_phone', 'get_created_at')  
+    search_fields = ('attendee__surname', 'attendee__other_name', 'attendee__email', 'attendee__phone', 'attendee__cys_code')
+    
+    def get_surname(self, obj):
+        return obj.attendee.surname
+    get_surname.short_description = 'Surname'
+    
+    def get_other_name(self, obj):
+        return obj.attendee.other_name
+    get_other_name.short_description = 'Other Name'
+
+    def get_email(self, obj):
+        return obj.attendee.email
+    get_email.short_description = 'Email'
+
+    def get_phone(self, obj):
+        return obj.attendee.phone
+    get_phone.short_description = 'Phone'
+
+    def get_created_at(self, obj):
+        return obj.attendee.created_at
+    get_created_at.short_description = 'Registered At'
