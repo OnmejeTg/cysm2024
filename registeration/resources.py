b146f7bf032768819c987ac68082d40069a6a0a4
Terdoo Nelson <nelsonterdoo1000@gmail.com>
2026-01-04 18:09:31 +0100

    fixed the bug causing the 500 error in the export process

--- registeration/resources.py
+++ registeration/resources.py
@@ -1,14 +1,19 @@
-from import_export import resources
+from import_export import resources, fields
 from .models import Attendee, Registrar
 
 class AttendeeResource(resources.ModelResource):
     class Meta:
         model = Attendee
-        fields = ('surname', 'other_name', 'email', 'phone', 'cys_code','church') 
-        export_order = ('surname', 'other_name', 'email', 'phone', 'cys_code')  
+        fields = ('surname', 'other_name', 'email', 'phone', 'cys_code', 'church') 
+        export_order = ('surname', 'other_name', 'email', 'phone', 'cys_code', 'church')  
 
 class RegistrarResource(resources.ModelResource):
+    surname = fields.Field(attribute='attendee__surname', column_name='Surname')
+    other_name = fields.Field(attribute='attendee__other_name', column_name='Other Name')
+    email = fields.Field(attribute='attendee__email', column_name='Email')
+    phone = fields.Field(attribute='attendee__phone', column_name='Phone')
+
     class Meta:
         model = Registrar
-        fields = ('surname', 'other_name', 'email', 'phone')
-        export_order = ('surname', 'other_name', 'email', 'phone')
+        fields = ('surname', 'other_name', 'email', 'phone', 'reg_id')
+        export_order = ('surname', 'other_name', 'email', 'phone', 'reg_id')
